c22c13aead8a5548ca465522d5cc4db41eda4412
Rubi Kuo <rubitiara@gmail.com>
2019-11-17 11:52:46 +0100

    develop js code

--- app.js
+++ app.js
@@ -5,19 +5,10 @@ const btnO = document.getElementById("btnO");
 const btnX = document.getElementById("btnX");
 const resetBtn = document.getElementById("resetBtn");
 
-const box1 = document.getElementById("1").textContent;
-const box2 = document.getElementById("2").textContent;
-const box3 = document.getElementById("3").textContent;
-const box4 = document.getElementById("4").textContent;
-const box5 = document.getElementById("5").textContent;
-const box6 = document.getElementById("6").textContent;
-const box7 = document.getElementById("7").textContent;
-const box8 = document.getElementById("8").textContent;
-const box9 = document.getElementById("9").textContent;
-
 let turn = true;
 let choose = false;
 let turntimes = 0;
+let win = false;
 
 bigctn.addEventListener("click", checkchoose);
 
@@ -44,13 +35,21 @@ function startWithO(e) {
     } else if (e.target.firstChild.textContent === "" && turn === true) {
       e.target.firstChild.textContent = "O";
       turn = false;
-
       turntimes++;
+      findWinner();
+      if (win === true) {
+        console.log("ok");
+        return;
+      }
     } else {
       e.target.firstChild.textContent = "X";
       turn = true;
       turntimes++;
-      console.log("turnTimes:" + turntimes);
+      findWinner();
+      if (win === true) {
+        console.log("ok");
+        return;
+      }
     }
   }
 }
@@ -61,6 +60,7 @@ btnX.addEventListener("click", function(e) {
   for (let smlCtn of smlCtns) {
     smlCtn.addEventListener("click", startWithX);
   }
+
 });
 
 function startWithX(e) {
@@ -73,12 +73,17 @@ function startWithX(e) {
       e.target.firstChild.textContent = "X";
       turn = false;
       turntimes++;
+      findWinner();
+      decoEmptyBox();
     } else {
       e.target.firstChild.textContent = "O";
       turn = true;
       turntimes++;
+      findWinner();
+      decoEmptyBox();
     }
   }
+ 
 }
 
 resetBtn.addEventListener("click", clearAll);
@@ -87,14 +92,116 @@ function clearAll(e) {
   for (let span of spans) {
     span.textContent = "";
   }
-  console.log(turntimes);
   turn = true;
   choose = false;
   turntimes = 0;
+  win = false;
   btnO.style.display = "inline";
   btnX.style.display = "inline";
   for (let smlCtn of smlCtns) {
     smlCtn.removeEventListener("click", startWithX);
     smlCtn.removeEventListener("click", startWithO);
+    //smlCtn.classList.remove("bkGrey");
   }
+}
+
+function findWinner() {
+  let box1 = document.getElementById("one");
+  let box2 = document.getElementById("two");
+  let box3 = document.getElementById("three");
+  let box4 = document.getElementById("four");
+  let box5 = document.getElementById("five");
+  let box6 = document.getElementById("six");
+  let box7 = document.getElementById("seven");
+  let box8 = document.getElementById("eight");
+  let box9 = document.getElementById("nine");
+  console.log(turntimes);
+  if (turntimes >= 4 && turntimes < 9 && win === false) {
+    if (
+      box1.textContent !== "" &&
+      box1.textContent === box2.textContent &&
+      box1.textContent === box3.textContent
+    ) {
+      console.log("win");
+
+      win = true;
+      return;
+    } else if (
+      box4.textContent !== "" &&
+      box4.textContent === box5.textContent &&
+      box4.textContent === box6.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box7.textContent !== "" &&
+      box7.textContent === box8.textContent &&
+      box7.textContent === box9.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box1.textContent !== "" &&
+      box1.textContent === box4.textContent &&
+      box1.textContent === box7.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box2.textContent !== "" &&
+      box2.textContent === box5.textContent &&
+      box2.textContent === box8.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box3.textContent !== "" &&
+      box3.textContent === box6.textContent &&
+      box3.textContent === box9.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box1.textContent !== "" &&
+      box1.textContent === box5.textContent &&
+      box1.textContent === box9.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } else if (
+      box3.textContent !== "" &&
+      box3.textContent === box5.textContent &&
+      box3.textContent === box7.textContent
+    ) {
+      console.log("win");
+      win = true;
+      return;
+    } 
+  }
+}
+
+function decoEmptyBox(){
+  if (win === true) {
+    let emptyboxs = Array.from(spans).filter(
+      span => span.textContent === ""
+    );
+    console.log(emptyboxs);
+    for (let emptybox of emptyboxs) {
+      emptybox.parentElement.removeEventListener("click", startWithX);
+     // emptybox.parentElement.classList.add("bkGrey");
+    }
+  }
+}
+
+function checkDraw(){
+   if(turntimes ===9 && win === false){
+     alert("draw");
+   }
+
 }

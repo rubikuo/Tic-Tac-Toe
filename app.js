e2e239fab577cb3ef96a37a0d88f2b2a4a90c85c
Rubi Kuo <rubitiara@gmail.com>
2019-11-20 10:09:54 +0100

    try welcome popup msg

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const body = document.querySelector("body");
 const bigctn = document.querySelector(".bigCtn");
 const smlCtns = document.querySelectorAll("div.smlCtn");
 const spans = document.querySelectorAll(".smlCtn span");
-const btnO = document.getElementById("btnO");
-const btnX = document.getElementById("btnX");
+const btnDiv = document.querySelector(".btnDiv");
+//const btnO = document.getElementById("btnO");
+//const btnX = document.getElementById("btnX");
 const restartBtn = document.getElementById("restartBtn");
 const resetBtn = document.getElementById("resetBtn");
 const msgDiv = document.querySelector(".msgDiv");
+ const onloadDiv = document.createElement("div");
 
 let turn = true;
 let choose = false;
@@ -17,36 +20,75 @@ let scoreP1 = 0;
 let scoreP2 = 0;
 let P1Type; // global variable for the playtype for later to sense who is the first player
 
-// to check if the user has chosen options
-bigctn.addEventListener("click", checkchoose);
+onload();
 
-function checkchoose() {
-  if (choose === false) {
-    alert("first player please choose 'O' or 'X' ");
-  }
+function onload(){
+ 
+  onloadDiv.classList.add("onloadPage");
+  const introDiv = document.createElement("div");
+  introDiv.classList.add("intro");
+  onloadDiv.appendChild(introDiv);
+  body.appendChild(onloadDiv);
+  const btnO = document.createElement("button");
+  btnO.id = "btnO";
+  btnO.textContent = "O Start";
+  const btnX = document.createElement("button");
+  btnX.id = "btnX";
+  btnX.textContent = "X Start";
+  introDiv.appendChild(btnX);
+  introDiv.appendChild(btnO);
+  btnX.addEventListener("click", onStartClick);
+  btnO.addEventListener("click", onStartClick);
+
 }
 
-btnX.addEventListener("click", onStartClick);
-btnO.addEventListener("click", onStartClick);
+// to check if the user has chosen X or O
+// bigctn.addEventListener("click", checkchoose);
+
+// function checkchoose() {
+//   if (choose === false) {
+//     alert("first player please choose 'O' or 'X' ");
+//   }
+// }
+
 
 function onStartClick(e) {
-  let otherBtn;
-  if (this.id === "btnO") {
-    otherBtn = btnX;
-    for (let smlCtn of smlCtns) {
-      smlCtn.addEventListener("click", startWithO);
+    if (this.id === "btnO") {
+    
+      for (let smlCtn of smlCtns) {
+        smlCtn.addEventListener("click", startWithO);
+      }
+    } else {
+      for (let smlCtn of smlCtns) {
+        smlCtn.addEventListener("click", startWithX);
+      }
     }
-  } else {
-    otherBtn = btnO;
-    for (let smlCtn of smlCtns) {
-      smlCtn.addEventListener("click", startWithX);
-    }
+    onloadDiv.classList.add("hide");
+   
+    // choose = true;
   }
-  otherBtn.style.display = "none";
-  choose = true;
-}
+// btnX.addEventListener("click", onStartClick);
+// btnO.addEventListener("click", onStartClick);
 
-//  because we merge two functions
+// function onStartClick(e) {
+//   let otherBtn;
+//   if (this.id === "btnO") {
+//     otherBtn = btnX;
+//     for (let smlCtn of smlCtns) {
+//       smlCtn.addEventListener("click", startWithO);
+//     }
+//   } else {
+//     otherBtn = btnO;
+//     for (let smlCtn of smlCtns) {
+//       smlCtn.addEventListener("click", startWithX);
+//     }
+//   }
+//   otherBtn.style.display = "none";
+//   choose = true;
+// }
+
+//  because we merge two functions with same concept but addeventListener to different btns so when user refresh, we must remove the eventListener otherwise 
+// when the user click on the other button on the second time, the other btn's function will not be triggered because the first btn has triggered the function so we must remove the first btn's event to make the event to happen on the second time 
 function startWithO(e) {
   startWith("O", e);
 }
@@ -61,7 +103,7 @@ function startWith(type, e) {
   } else {
     otherType = "X";
   }
-  P1Type = type; // this is already determining the type for the first player
+  P1Type = type; // because when user choose O or X, the one who choose is the first player so we know the type chosen by the firstplayer is the same type we set here
   if (choose === false) {
     return;
   } else {
@@ -83,11 +125,13 @@ function startWith(type, e) {
   }
 }
 
+// this btn is to refresh the window and reset everything so the score will not continue counting 
 resetBtn.addEventListener("click", function(){
  // alert("this will reset score to 0, are you sure?");
   location.reload();
 });
 
+// this btn is to clear the game board but not to reset everything so the score will continue counting if users play again
 restartBtn.addEventListener("click", clearAll);
 
 function clearAll(e) {
@@ -108,12 +152,13 @@ function clearAll(e) {
   msgDiv.style.display = "none";
   msgDiv.firstChild.textContent = "";
 
-  // to remove the animation when reset because the transition only occurs when the class is added so it must be reAdded everytime to the element
+  // to remove the animation when restart, because the animation only occurs when the class is added so it must be reAdded everytime to the element
   for (let el of document.querySelectorAll(".anima")) {
     el.classList.remove("anima");
   }
 }
 
+// find winner 
 function findWinner() {
   let xWon = checkIfWinner("X");
   let oWon = checkIfWinner("O");
@@ -145,6 +190,7 @@ function findWinner() {
   play2.textContent = scoreP2;
 }
 
+// check who is the winner
 function checkIfWinner(type) {
   console.log("check");
   let boxes = document.querySelectorAll(".smlCtn span");
@@ -173,6 +219,8 @@ function checkIfWinner(type) {
   return false;
 }
 
+// to stop users next movement once there is a won result
+//so we loop through the array of span to find the empty boxes
 function decoEmptyBox(result) {
   if (result === true) {
     let emptyboxs = Array.from(spans).filter(span => span.textContent === "");
@@ -184,6 +232,7 @@ function decoEmptyBox(result) {
   }
 }
 
+// to control the popiup message
 function showMsg(type, result) {
   if (
     (turntimes >= 4 && result === true) ||
